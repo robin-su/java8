bdb2271730a757b72a323f4bf5b00cc85e8b4e18
robin <549570881@qq.com>
2018-10-28 23:13:21 +0800

    java8 新特性学习

---
+++ src/main/java/com/myspring/nio/TestChannel.java
@@ -0,0 +1,242 @@
+package com.myspring.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+
+/*
+ * 一、通道（Channel）：用于源节点与目标节点的连接。在 Java NIO 中负责缓冲区中数据的传输。Channel 本身不存储数据，因此需要配合缓冲区进行传输。
+ *
+ * 二、通道的主要实现类
+ * 	java.nio.channels.Channel 接口：
+ * 		|--FileChannel
+ * 		|--SocketChannel
+ * 		|--ServerSocketChannel
+ * 		|--DatagramChannel
+ *
+ * 三、获取通道
+ * 1. Java 针对支持通道的类提供了 getChannel() 方法
+ * 		本地 IO：
+ * 		FileInputStream/FileOutputStream
+ * 		RandomAccessFile
+ *
+ * 		网络IO：
+ * 		Socket
+ * 		ServerSocket
+ * 		DatagramSocket
+ *
+ * 2. 在 JDK 1.7 中的 NIO.2 针对各个通道提供了静态方法 open()
+ * 3. 在 JDK 1.7 中的 NIO.2 的 Files 工具类的 newByteChannel()
+ *
+ * 四、通道之间的数据传输
+ * transferFrom()
+ * transferTo()
+ *
+ * 五、分散(Scatter)与聚集(Gather)
+ * 分散读取（Scattering Reads）：将通道中的数据分散到多个缓冲区中(按照缓冲区的顺序，从 Channel 中读取的数据依次将 Buffer 填满)
+ * 聚集写入（Gathering Writes）：将多个缓冲区中的数据聚集到通道中(按照缓冲区的顺序，写入 position 和 limit 之间的数据到 Channel 。)
+ *
+ * 六、字符集：Charset
+ * 编码：字符串 -> 字节数组
+ * 解码：字节数组  -> 字符串
+ *
+ */
+public class TestChannel {
+
+    public static void main(String[] args) throws Exception {
+//        testCopyWithChannel();
+//        testCopyWithDirectCache();
+//        testCopyWithRChannel();
+//        testScatterAndGather();
+//        testCharset();
+    }
+
+    public static void testEncoderAndDecoder() throws IOException{
+        Charset cs1 = Charset.forName("GBK");
+        //获取编码器
+        CharsetEncoder ce = cs1.newEncoder();
+        //获取解码器
+        CharsetDecoder cd = cs1.newDecoder();
+
+        CharBuffer cBuf = CharBuffer.allocate(1024);
+        cBuf.put("尚硅谷威武！");
+        cBuf.flip();
+
+        //编码
+        ByteBuffer bBuf = ce.encode(cBuf);
+        for(int i=0;i<12;i++) { //一个中文占2字节，所以是12
+            System.out.println(bBuf.get());
+        }
+
+        //解码
+        bBuf.flip();
+        CharBuffer cBuf2 = cd.decode(bBuf);
+        System.out.println(cBuf2.toString());
+
+        System.out.println("------------------------------------------------------");
+
+        Charset cs2 = Charset.forName("GBK");
+        bBuf.flip();
+        CharBuffer cBuf3 = cs2.decode(bBuf);
+        System.out.println(cBuf3.toString());
+    }
+
+    public static void testCharset() {
+        SortedMap<String, Charset> map = Charset.availableCharsets();
+        Set<Map.Entry<String, Charset>> set = map.entrySet();
+        for(Map.Entry<String,Charset> entry : set) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+    }
+
+    public static void testScatterAndGather() throws IOException{
+        RandomAccessFile raf1 = new RandomAccessFile("D:\\workspace\\study\\java8\\data\\1.txt", "rw");
+
+        //1. 获取通道
+        FileChannel channel1 = raf1.getChannel();
+
+        //2. 分配指定大小的缓冲区
+        ByteBuffer buf1 = ByteBuffer.allocate(100);
+        ByteBuffer buf2 = ByteBuffer.allocate(1024);
+
+        //3. 分散读取
+        ByteBuffer[] bufs = {buf1, buf2};
+        channel1.read(bufs);
+
+        for (ByteBuffer byteBuffer : bufs) {
+            byteBuffer.flip();
+        }
+
+        System.out.println(new String(bufs[0].array(), 0, bufs[0].limit()));
+        System.out.println("-----------------");
+        System.out.println(new String(bufs[1].array(), 0, bufs[1].limit()));
+
+        //4. 聚集写入
+        RandomAccessFile raf2 = new RandomAccessFile("D:\\workspace\\study\\java8\\data\\2.txt", "rw");
+        FileChannel channel2 = raf2.getChannel();
+
+        channel2.write(bufs);
+
+    }
+
+
+    //通道之间的数据传输（直接缓冲区）
+    public static void testCopyWithRChannel() throws Exception {
+        FileChannel inChannel = FileChannel.open(Paths.get("D:\\workspace\\study\\java8\\data\\1.jpg"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("D:\\workspace\\study\\java8\\data\\4.jpg"),
+                StandardOpenOption.WRITE,
+                StandardOpenOption.READ,
+                StandardOpenOption.CREATE_NEW);
+        //		inChannel.transferTo(0, inChannel.size(), outChannel);
+        outChannel.transferFrom(inChannel, 0, inChannel.size());
+
+        inChannel.close();
+        outChannel.close();
+    }
+
+    //使用直接缓冲区完成文件的复制(内存映射文件)
+    public static void testCopyWithDirectCache() throws Exception {
+        long start = System.currentTimeMillis();
+
+        FileChannel inChannel = FileChannel.open(Paths.get("D:\\workspace\\study\\java8\\data\\1.jpg"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("D:\\workspace\\study\\java8\\data\\3.jpg"),
+                StandardOpenOption.WRITE,StandardOpenOption.READ,StandardOpenOption.CREATE_NEW);
+
+        //内存映射文件
+        MappedByteBuffer inMappedBuf = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+        //这个内存中要求的是读写模式，因此outChannel也应该支持读写模式
+        MappedByteBuffer outMappedBuf = outChannel.map(FileChannel.MapMode.READ_WRITE, 0, inChannel.size());
+
+        //直接对缓冲区进行数据的读写操作
+        byte[] dst = new byte[inMappedBuf.limit()];
+        inMappedBuf.get(dst);
+        outMappedBuf.put(dst);
+
+        inChannel.close();
+        outChannel.close();
+
+        long end = System.currentTimeMillis();
+        System.out.println("耗费时间为：" + (end - start));
+    }
+
+    public static void testCopyWithChannel() {
+        long start = Instant.now().toEpochMilli();
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        //①获取通道
+        FileChannel inChannel = null;
+        FileChannel outChannel = null;
+
+        try {
+            fis = new FileInputStream("D:\\workspace\\study\\java8\\data\\1.jpg");
+            fos = new FileOutputStream("D:\\workspace\\study\\java8\\data\\2.jpg");
+
+            inChannel = fis.getChannel();
+            outChannel = fos.getChannel();
+
+            //②分配指定大小的缓冲区
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+
+            //③将通道中的数据存入缓冲区中
+            while(inChannel.read(buf) != -1) {
+                buf.flip();//切换读取数据的模式
+                //④将缓冲区中的数据写入通道中
+                outChannel.write(buf);
+                buf.clear();//清空缓冲区
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(outChannel != null){
+                try {
+                    outChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(inChannel != null){
+                try {
+                    inChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        long end = Instant.now().toEpochMilli();
+        System.out.println("耗费时间为：" + (end - start));
+    }
+
+}
